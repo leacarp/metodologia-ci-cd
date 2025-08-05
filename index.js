6ab21fb3c3fbd6646593956bf66ceb46fe519f6d
Leandro <leanaguirre2016@gmail.com>
2025-08-04 21:33:36 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Primer endpoint GET
+app.get('/mensaje1', (req, res) => {
+  res.send('Hola desde el primer endpoint');
+});
+
+// Segundo endpoint GET
+app.get('/mensaje2', (req, res) => {
+  res.send('Hola desde el segundo endpoint');
+});
+
+// Iniciar servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
